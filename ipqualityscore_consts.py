84b4c19c9f5118d739cc5cc9bb961fd48caf43c8
Rambatla Venkat Rao <68921481+RamboV@users.noreply.github.com>
2023-05-30 18:02:19 +0530

    PAPP-30768: ipqualityscore - corrected error messages in check_url (#4)
    * corrected error messages in check_url
    
    * change min_phantom_version
    
    * dev check, static job changes and pre-commit changes
    
    * consistent variable and json changes
    
    * added fips complient and update app version
    
    * added license in py files and updated year
    
    * change min phantom version
    
    ---------
    
    Co-authored-by: aparekh-crest <aparekh@splunk.com>

--- ipqualityscore_consts.py
+++ ipqualityscore_consts.py
@@ -1,6 +1,17 @@
 # File: ipqualityscore_consts.py
 #
-# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+# Copyright (c) 2021-2023 IPQualityScore.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
 
 IPQUALITYSCORE_DOMAIN = 'https://ipqualityscore.com'
 
@@ -10,20 +21,20 @@ IPQUALITYSCORE_API_IP_REPUTATION = 'https://ipqualityscore.com/api/json/ip/{apik
 IPQUALITYSCORE_API_EMAIL_VALIDATION = 'https://ipqualityscore.com/api/json/email/{apikey}/{email}'
 
 IPQUALITYSCORE_APP_KEY = 'app_key'
-IPQUALITYSCORE_MSG_QUERY_URL = 'Querying URL: {query_url}'
-IPQUALITYSCORE_MSG_CONNECTING = 'Polling IPQualityScore site ...'
-IPQUALITYSCORE_SERVICE_SUCC_MSG = 'IPQualityScore Service successfully executed.'
-IPQUALITYSCORE_SUCC_CONNECTIVITY_TEST = 'Test Connectivity passed'
-IPQUALITYSCORE_ERR_CONNECTIVITY_TEST = 'Test Connectivity failed'
-IPQUALITYSCORE_MSG_GOT_RESP = 'Got response from IPQualityScore'
+IPQUALITYSCORE_MESSAGE_QUERY_URL = 'Querying URL: {query_url}'
+IPQUALITYSCORE_MESSAGE_CONNECTIVITY = 'Polling IPQualityScore site ...'
+IPQUALITYSCORE_SERVICE_SUCCESS_MESSAGE = 'IPQualityScore Service successfully executed.'
+IPQUALITYSCORE_SUCCESS_CONNECTIVITY_TEST = 'Test Connectivity passed'
+IPQUALITYSCORE_ERROR_CONNECTIVITY_TEST = 'Test Connectivity failed'
+IPQUALITYSCORE_MESSAGE_GOT_RESP = 'Got response from IPQualityScore'
 IPQUALITYSCORE_NO_RESPONSE = 'Server did not return a response \
                          for the object queried'
-IPQUALITYSCORE_SERVER_CONNECTION_ERR = 'Server connection error'
-IPQUALITYSCORE_MSG_CHECK_CONNECTIVITY = 'Please check your network connectivity'
-IPQUALITYSCORE_SERVER_RETURNED_ERR_CODE = 'Server returned error code: {code}'
-IPQUALITYSCORE_ERR_MSG_OBJECT_QUERIED = "IPQualityScore response didn't \
+IPQUALITYSCORE_SERVER_CONNECTIVITY_ERROR = 'Server connection error'
+IPQUALITYSCORE_MESSAGE_CHECK_CONNECTIVITY = 'Please check your network connectivity'
+IPQUALITYSCORE_SERVER_RETURNED_ERROR_CODE = 'Server returned error code: {code}'
+IPQUALITYSCORE_ERROR_MESSAGE_OBJECT_QUERIED = "IPQualityScore response didn't \
                                     send expected response"
-IPQUALITYSCORE_SERVER_ERR_RATE_LIMIT = 'Query is being rate limited. \
+IPQUALITYSCORE_SERVER_ERROR_RATE_LIMIT = 'Query is being rate limited. \
                                      Server returned 509'
 
 ACTION_ID_URL_CHECKER = 'check_url'
@@ -31,14 +42,17 @@ ACTION_ID_IP_REPUTATION = 'ip_reputation'
 ACTION_ID_EMAIL_VALIDATION = 'email_validation'
 
 # Constants relating to '_get_error_message_from_exception'
-ERR_CODE_MSG = "Error code unavailable"
-ERR_MSG_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters"
-PARSE_ERR_MSG = "Unable to parse the error message. Please check the asset configuration and|or action parameters"
+ERROR_CODE_MESSAGE = "Error code unavailable"
+ERROR_MESSAGE_UNAVAILABLE = "Error message unavailable. Please check the asset configuration and|or action parameters"
+PARSE_ERROR_MESSAGE = "Unable to parse the error message. Please check the asset configuration and|or action parameters"
 
 # Constants relating to '_validate_integer'
-VALID_INTEGER_MSG = "Please provide a valid integer value in the {}"
-NON_NEGATIVE_INTEGER_MSG = "Please provide a valid non-negative integer value in the {}"
+VALID_INTEGER_MESSAGE = "Please provide a valid integer value in the {}"
+NON_NEGATIVE_INTEGER_MESSAGE = "Please provide a valid non-negative integer value in the {}"
 TIMEOUT_KEY = "'timeout' action parameter"
 STRICTNESS_KEY = "'strictness' action parameter"
 ABUSE_STRICTNESS_KEY = "'abuse_strictness' action parameter"
 TRANSACTION_STRICTNESS_KEY = "'transaction_strictness' action parameter"
+
+# timeout
+IPQUALITYSCORE_DEFAULT_TIMEOUT = 30
